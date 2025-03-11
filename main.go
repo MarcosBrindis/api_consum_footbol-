736762d3305a674a67c3b977ad84741e8e87708d
MarcosBrindis <markos_brind95@hotmail.com>
2025-03-10 23:36:41 -0600

    despliegue

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"consumer/src/consumer/infrastructure"
 	"consumer/src/consumer/infrastructure/router"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -13,6 +15,17 @@ func main() {
 
 	// Crear la instancia del router de Gin
 	r := gin.Default()
+
+	// Configurar CORS
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		AllowOriginFunc:  func(origin string) bool { return true }, // Permite cualquier origen
+		MaxAge:           12 * time.Hour,
+	}))
 
 	// Configurar las rutas para el consumidor
 	router.SetupConsumerRoutes(r, infrastructure.ConsumeSportEventController)
